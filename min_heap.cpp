0dc6b000a3867fc8e30517254af94ea0103de75f
VAIBHAVBAJPAI1402 <bajpai.vaibhav47@gmail.com>
2020-02-04 22:24:57 +0530

    Fourth commit, date 4-feb-2020

--- min_heap.cpp
+++ min_heap.cpp
@@ -26,6 +26,9 @@ class MinHeap
 		}
 		void insert(int k);
 		void Heapify(int i);
+		void deletekey(int i);
+		int extractMin();
+		void decreasekey(int val, int i);
 		int parent(int i)
 		{
 			return (i-1)/2;
@@ -44,29 +47,29 @@ void MinHeap::Heapify(int i)
 	int l=left_child(i);
 	int r=right_child(i);
 	int small=i;
-	while(l<heap_size && harr[l]<harr[small])
+	while(l<heap_size && harr[l]<harr[i])
 	{
 		small=l;
 	}
-	while(r<heap_size && harr[r]<harr[l])
+	while(r<heap_size && harr[r]<harr[small])
 	{
 		small=r;
 	}
 	if(small!=i)
 	{
 		swap(&harr[small],&harr[i]);
-		Heapify(parent(i));
+		Heapify(small);
 	}
 }
 void MinHeap::insert(int k)
 {
-	if(heap_size>=capacity)
+	if(heap_size==capacity)
 	{
 		cout<<"OverFlow";
 		return;
 	}
 	heap_size++;
-	int i=heap_size--;
+	int i=heap_size-1;
 	harr[i]=k;
 	while(i!=0 && harr[parent(i)]>harr[i])
 	{
@@ -74,13 +77,51 @@ void MinHeap::insert(int k)
 		i=parent(i);
 	}
 }
+void MinHeap::decreasekey(int val,int i)
+{
+	harr[i]=val;
+	while(i!=0 && harr[i]<harr[parent(i)])
+	{
+		swap(&harr[i],&harr[parent(i)]);
+		i=parent(i);
+	}
+}
+int MinHeap::extractMin()
+{
+	if(heap_size==0)
+	{
+		return INT_MAX;
+	}
+	if(heap_size==1)
+	{
+		heap_size--;
+		return harr[0];
+	}
+	
+	//if heap has more than 1 element
+	int root=harr[0];			//store 
+	harr[0]=harr[heap_size-1];	//store max value in place of root and heapify it
+	heap_size--;
+	Heapify(0);					//heapify the heap 
+	return root;
+	
+}
+void MinHeap::deletekey(int i)
+{
+	decreasekey(INT_MIN, i);
+	extractMin();
+}
 int main()
 {
-	MinHeap mh(5);
-	mh.insert(12);
+	MinHeap mh(10);
+	mh.insert(10);
 	mh.insert(5);
-	mh.insert(99);
+	mh.insert(101);
 	cout<<mh.getMin()<<"\t";
 	mh.insert(1);
+	cout<<mh.getMin()<<"\t";
+	mh.decreasekey(12,2);
+	cout<<mh.extractMin()<<"\t";
+	mh.deletekey(2);
 	cout<<mh.getMin()<<"\t";
 }
